7854862a96659cde3802c3aa02d314c70bb3e26e
triploit <tristanpieper080803@gmail.com>
2017-04-20 01:49:24 +0200

    Updated

--- src/io/github/triploit/Main.java
+++ src/io/github/triploit/Main.java
@@ -4,12 +4,15 @@ import io.github.triploit.parser.Parser;
 import io.github.triploit.parser.Tokenizer;
 
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * Created by survari on 14.04.17.
  */
 public class Main
 {
+    public static ArrayList<String> files = new ArrayList<>();
+
 	public static void main(String[] args)
 	{
 		if (args.length < 1)
@@ -21,35 +24,11 @@ public class Main
 		{
 			for (String arg : args)
 			{
+			    files.clear();
 				String code = "";
-				System.out.println("Processing "+arg+" ...");
+				System.out.println("MAIN: Processing "+arg+" ...");
 
-				try
-				{
-					if ((new File(arg)).isDirectory())
-					{
-						System.out.println("Error: The \"file\" is a directory!");
-						return;
-					}
-
-					BufferedReader br = new BufferedReader(new FileReader(arg));
-					String line;
-
-					while ((line = br.readLine()) != null)
-					{
-						code = code + line + "\n";
-					}
-				}
-				catch (FileNotFoundException e)
-				{
-					System.out.println("Error: File not found!");
-					return;
-				}
-				catch (IOException e)
-				{
-					System.out.println("Error: You don't have reading permissions!");
-					return;
-				}
+				code = _read_file(arg);
 
 				Parser.parse(Tokenizer.tokenize(code));
 				code = Parser.code;
@@ -61,19 +40,73 @@ public class Main
 				System.out.println(code);
 				System.out.println("[]=====================[ CODE END: "+arg+" ]=====================[]"); */
 
-				System.out.println("Saved in: "+arg.replace(".aml", ".html"));
+				System.out.println("MAIN: Saved in: "+arg+".out.html!");
 
 				try
 				{
-					BufferedWriter bw = new BufferedWriter(new FileWriter(arg.replace(".aml", ".html")));
+					BufferedWriter bw = new BufferedWriter(new FileWriter(arg+".out.html"));
 					bw.write(code);
 					bw.close();
 				}
 				catch (IOException e)
 				{
-					System.out.println("Error: You don't have writing permissions!");
+					System.out.println("MAIN: Error: You don't have writing permissions!");
 				}
 			}
 		}
+	}
+
+	private static String _read_file(String arg)
+	{
+		String code = "";
+		File file = (new File(arg));
+
+		try
+		{
+			if (file.isDirectory())
+			{
+				System.out.println("FILE_READER: Error: The \"file\" is a directory!");
+				return "";
+			}
+
+			BufferedReader br = new BufferedReader(new FileReader(arg));
+			String line;
+
+			while ((line = br.readLine()) != null)
+			{
+			    if (line.trim().startsWith("#!(inc: "))
+                {
+                    // 8 to (len-1)
+                    String f = line.trim().substring(8, line.trim().length()-1);
+                    f = file.getAbsolutePath().substring(0, file.getAbsolutePath().length()-arg.split("/")[arg.split("/").length-1].length()-1)+"/"+f;
+
+                    System.out.println("Reading "+f+"...");
+
+                    if (!files.contains(f))
+                    {
+                        files.add(f);
+                        code = code + _read_file(f);
+                    }
+                    else
+                    {
+                        System.out.println("FILE_READER: Warning: File ignored!");
+                    }
+                }
+                else
+				    code = code + line + "\n";
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("FILE_READER: Error: File not found!");
+			return "";
+		}
+		catch (IOException e)
+		{
+			System.out.println("FILE_READER: Error: You don't have reading permissions!");
+			return "";
+		}
+
+		return code;
 	}
 }
